18ba21b47958f3952cbb7e1de48159448248ec9e
john <wayaway.jtm@gmail.com>
2020-03-09 19:27:34 -0400

    Generating components

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { FactsPageComponent } from './facts-page/facts-page.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    FactsPageComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
