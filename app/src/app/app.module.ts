f610e22591c6d9ce1728bcb3608f9bfa0f28d2dc
SpencerLaptop <spencer23559@gmail.com>
2020-03-09 20:04:51 -0400

    angular 9

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps'
 import { AppRoutingModule } from './app-routing.module';
@@ -9,7 +8,6 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
     GoogleMapsModule
   ],
