8cb3baadfc55a18b58ee8fece60296680393dbfa
SpencerLaptop <spencer23559@gmail.com>
2020-03-09 21:40:33 -0400

    map working

--- app/src/app/map/map.component.ts
+++ app/src/app/map/map.component.ts
@@ -5,12 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
+
 export class MapComponent implements OnInit {
-  
+  longitude=-85.6681
+  latitude=42.9634
   constructor() { }
 
   ngOnInit() {
-    
+   
   }
 
 }
