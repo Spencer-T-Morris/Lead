4b789d0389094171e536361723041a16d21bf055
SpencerLaptop <spencer23559@gmail.com>
2020-03-09 20:55:17 -0400

    maps now works

--- app/src/app/map/map.component.ts
+++ app/src/app/map/map.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
