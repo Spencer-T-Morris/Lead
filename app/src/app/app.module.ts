a72be64ab830fd7cca7d880a45782e99761aa536
Nyki <nykilundgren127@gmail.com>
2020-03-11 21:31:07 -0400

    creating HTML, putting initial info in

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FactsPageComponent } from './facts-page/facts-page.component';
 import { MapComponent } from './map/map.component';
-import { BrowserModule } from '@angular/platform-browser'
+import { BrowserModule } from '@angular/platform-browser';
+import { AssitanceComponent } from './assistance/assitance.component'
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     FactsPageComponent,
-    MapComponent
+    MapComponent,
+    AssitanceComponent
   ],
   imports: [
     AppRoutingModule,
