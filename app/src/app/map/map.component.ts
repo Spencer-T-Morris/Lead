e43d83910f76174321660b7b651aae775a7ea20e
john <wayaway.jtm@gmail.com>
2020-03-16 18:17:15 -0400

    moved map height & width to variables

--- app/src/app/map/map.component.ts
+++ app/src/app/map/map.component.ts
@@ -10,6 +10,9 @@ export class MapComponent implements OnInit {
   LatLng= new google.maps.LatLng({lng:-85.6681, lat: 42.9634})
   zoom= 10
   GR= "grand rapids, michigan"
+  public mapHeight: string = "75vh";
+  public mapWidth: string = "90vw";
+
   constructor() { }
 
   ngOnInit() {
