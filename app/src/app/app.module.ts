dc79bdf7bba0252c1a1d3b7d44d56d7c324e9160
john <wayaway.jtm@gmail.com>
2020-03-18 14:44:42 -0400

    Init Address Search work

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -5,11 +5,12 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { FactsPageComponent } from './facts-page/facts-page.component';
 import { MapComponent } from './map/map.component';
+import { AssitanceComponent } from './assistance/assitance.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatExpansionModule } from "@angular/material/expansion";
-import { AssitanceComponent } from './assistance/assitance.component';
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { AssitanceComponent } from './assistance/assitance.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatSidenavModule,
-    MatExpansionModule
+    MatExpansionModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- app/src/app/map/map.component.ts
+++ app/src/app/map/map.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { GoogleMap } from '@angular/google-maps';
 
 @Component({
   selector: 'app-map',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MapComponent implements OnInit {
+  @ViewChild('googleMap') gMap: google.maps.Map;
+  // gMap: google.maps.Map;
   LatLng = new google.maps.LatLng({ lng: -85.6681, lat: 42.9634 });
+  geocoder = new google.maps.Geocoder();
   zoom = 11;
   GR = "grand rapids, michigan";
   public mapHeight: string = "75vh";
@@ -19,7 +23,7 @@ export class MapComponent implements OnInit {
     scrollwheel: true,
     disableDoubleClickZoom: false,
     maxZoom: 20,
-    minZoom: 10,
+    minZoom: 10
   };
 
   circles = [{
@@ -40,5 +44,44 @@ export class MapComponent implements OnInit {
     if (this.zoom > this.options.minZoom) this.zoom--
   }
 
-  ngOnInit() { }
+  focusOnAddress(address: string) {
+    console.log('Focusing on', address);
+    this.centerMapOnAddress(this.geocoder, this.gMap, address);
+    // console.log(this.gMap);
+    // this.geocoder.geocode({ 'address': address }, function (results, status) {
+    //   if (status == 'OK') {
+    //     if ( false ) {
+    //       // this.gMap === undefined
+    //       console.log('gMap is undefined');
+    //     } else {
+    //       this.gMap.center = results[0].geometry.location;
+    //       this.gMap.zoom = 16;
+    //     }
+    //   } else {
+    //     console.log('Geocode was not successful for the following reason: ' + status);
+    //   }
+    // });
+  }
+
+  logCenter() {
+    console.log(this.gMap.getCenter().toString());
+  }
+
+  centerMapOnAddress(newGeocoder, targetMap, address) {
+    newGeocoder.geocode({ 'address': address }, function (results, status) {
+      if (status == 'OK') {
+        if (targetMap === undefined) {
+          console.log('gargetMap is undefined');
+        } else {
+          targetMap.center = results[0].geometry.location;
+          targetMap.zoom = 16;
+        }
+      } else {
+        console.log('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
+
+  ngOnInit() {
+  }
 }
