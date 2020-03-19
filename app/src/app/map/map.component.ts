705afe653a1b9a69b9e32bc4f943f9d6df737727
SpencerLaptop <spencer23559@gmail.com>
2020-03-18 21:28:36 -0400

    comments out heat map for demo

--- app/src/app/map/map.component.ts
+++ app/src/app/map/map.component.ts
@@ -68,13 +68,13 @@ export class MapComponent implements OnInit {
   logCenter() {
     console.log(this.gMap.getCenter().toString());
   }
-  heatmap = new google.maps.visualization.HeatmapLayer({
-    data: this.getCityData(),
+  //heatmap = new google.maps.visualization.HeatmapLayer({
+    //data: this.getCityData(),
     //map: Map
-  });
-  getCityData(){
+  //});
+  //getCityData(){
 
-  }
+  //}
   ngOnInit() {
   }
 }
