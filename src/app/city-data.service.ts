5fc425e9f8c20748026977a5784aab21e6fe3f52
john <wayaway.jtm@gmail.com>
2020-03-26 15:41:59 -0400

    Implemented pulling city data from proxy

--- src/app/city-data.service.ts
+++ src/app/city-data.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
 import { CityResidence } from './cityResidence';
 import { HttpClient } from '@angular/common/http'; // For pulling & authenticating from API
-import * as geocodedCityData from '../assets/gr_parcels_geocoded.json';
+// For previous versions that pulled from a local file
+// import * as geocodedCityData from '../assets/gr_parcels_geocoded.json';
+
+// The following is referred to as LOCAL DATA COMMENT
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  For previous versions that pulled from a local file
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 @Injectable({
   providedIn: 'root'
@@ -9,27 +15,33 @@ import * as geocodedCityData from '../assets/gr_parcels_geocoded.json';
 export class CityDataService {
 
   private cityData: CityResidence[] = [];
-  private tempData = (geocodedCityData as any).default;
+  // See LOCAL DATA COMMENT
+  // private tempData = (geocodedCityData as any).default;
   // ==============================================
   // Vars used for pulling & authenticating from API
   // ==============================================  
+  // These vars are for pulling from the proxy that BJ setup
+  private cityDataProxy: string = 'https://grand-rapids-proxy.herokuapp.com/parcels.json';
+  // The vars below are used for pulling directly from the city DB
   // private apiKey: string = 'dcqrbg7rl05yovr0t2www8v6t';
   // private apiPassword: string = '56j23yrxr77ozcmkpruk21qukq6rsk9voiffl6ppk4i4rhojhw';
   // private cityDataEndpt: string = 'https://data.grandrapidsmi.gov/resource/xbdc-f64e.json?$where=year_built%20%3C%20%271978%27%20AND%20year_built%20!=%20%270%27';
 
   // HttpClient used for pulling & authenticating from API
   constructor(private http: HttpClient) {
-    for (const residence of this.tempData) {
-      this.cityData.push(new CityResidence(residence));
-    }
+    console.log('Service loaded');
+    // See LOCAL DATA COMMENT
+    // for (const residence of this.tempData) {
+    //   this.cityData.push(new CityResidence(residence));
+    // }
     // ==============================================
-    // Placeholder for pulling & authenticating from API
+    // For pulling & authenticating from API
     // ==============================================
-    // this.http.get(this.cityDataEndpt).subscribe((data: any) => {
-    //   for (const residence of data) {
-    //     this.cityData.push(new CityResidence(residence));
-    //   }
-    // });
+    this.http.get(this.cityDataProxy).subscribe((data: any) => {
+      for (const residence of data) {
+        this.cityData.push(new CityResidence(residence));
+      }
+    });
   }
 
   /**
